40e514ef23358d0fd14eb8b3c375f0839d4ad791
Nadezda Grozdova <nadezda.grozdova@gmail.com>
2023-07-14 17:53:16 +0300

    First version of Amazing numbers 1/8

--- Amazing Numbers/task/src/numbers/Main.java
+++ Amazing Numbers/task/src/numbers/Main.java
@@ -1,52 +1,47 @@
 
 package numbers;
 
-import java.util.EnumSet;
 import java.util.Scanner;
-import java.util.Set;
 
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        Set<Properties> availableProperties = EnumSet.allOf(Properties.class);
-        MenuService.printWelcome();
-        MenuService.printSupportedRequests();
-        boolean runProgram = true;
+        System.out.println("Enter a natural number:");
+        int userNumber = scanner.nextInt();
+        if (userNumber <= 0) {
+            System.out.println("This number is not natural!");
+        } else {
+            parityOfNumber(userNumber);
+            isABuzzNumber(userNumber);
+        }
+    }
 
-        while (runProgram) {
-            System.out.print("\nEnter a request: ");
-            String userInput = scanner.nextLine();
-            if (userInput == "") {
-                MenuService.printSupportedRequests();
-                continue;
-            }
-            String[] splitedInput = userInput.split(" ");
-            int inputLength = splitedInput.length;
-            String userStartNumber = splitedInput[0];
-            Long userNumber = Long.parseLong(userStartNumber);
-            if (userNumber < 0) {
-                System.out.println("\nThe first parameter should be a natural number or zero.");
-                continue;
-            }
-            if (userNumber == 0) {
-                runProgram = false;
-                System.out.println("\nGoodbye!");
-                break;
-            }
-            switch (inputLength) {
-                case 1:
-                    PrintResultService.printOneNumberResult(userNumber, userStartNumber);
-                    break;
-                case 2:
-                    MenuService.resultsForTwoInputs(splitedInput, userNumber);
-                    break;
-                case 3:
-                    MenuService.resultsForThreeInputs(availableProperties, splitedInput, userNumber);
-                    break;
-                default:
-                    MenuService.resultsForMoreInputs(splitedInput, userNumber, availableProperties);
-                    break;
-            }
+    private static void parityOfNumber(int userNumber) {
+        if (userNumber % 2 == 0) {
+            System.out.println("This number is Even.");
+        } else {
+            System.out.println("This number is Odd.");
+        }
+    }
+
+    private static void isABuzzNumber(int userNumber) {
+//        int theLastDigit = Integer.parseInt(String.valueOf(userInput.charAt(userInput.length() - 1)));
+        if (userNumber % 7 == 0 && userNumber % 10 == 7) {
+            System.out.println("It is a Buzz number.");
+            System.out.println("Explanation:\n" +
+                    userNumber + " is divisible by 7 and ends with 7.");
+        } else if (userNumber % 7 == 0 && userNumber % 10 != 7) {
+            System.out.println("It is a Buzz number.");
+            System.out.println("Explanation:\n" +
+                    userNumber + " is divisible by 7.");
+        } else if (userNumber % 7 != 0 && userNumber % 10 == 7) {
+            System.out.println("It is a Buzz number.");
+            System.out.println("Explanation:\n" +
+                    userNumber + " ends with 7.");
+        } else {
+            System.out.println("It is not a Buzz number.");
+            System.out.println("Explanation:\n" +
+                    userNumber + " is neither divisible by 7 nor does it end with 7.");
         }
     }
 }
--- Amazing Numbers/task/src/numbers/Properties.java
+++ Amazing Numbers/task/src/numbers/Properties.java
@@ -1,18 +1,10 @@
 package numbers;
 
 public enum Properties {
-    EVEN(" even"),
-    ODD(" odd"),
-    BUZZ(" buzz"),
-    DUCK(" duck"),
-    PALINDROMIC(" palindromic"),
-    GAPFUL(" gapful"),
-    SPY(" spy"),
-    SQUARE (" square"),
-    SUNNY (" sunny"),
-    JUMPING (" jumping"),
-    HAPPY (" happy"),
-    SAD(" sad");
+    EVEN("even: "),
+    ODD("odd: "),
+    BUZZ("buzz: "),
+    DUCK("duck: ");
 
     private final String text;
 
@@ -22,15 +14,5 @@ public enum Properties {
 
     public String getText() {
         return text;
-    }
-
-    public static boolean exists(String name) {
-        for (Properties p: values()) {
-            if (p.name().equals(name)) {
-                return true;
-            }
-        }
-
-        return false;
     }
 }
